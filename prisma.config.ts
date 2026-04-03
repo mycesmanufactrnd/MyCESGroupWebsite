604c381c7fdcb481a469bddb956ec2a4f0c2c965
mycesmanufactrnd <mycesmanufactrnd@gmail.com>
2026-04-03 09:57:52 +0800

    test

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import 'dotenv/config' // This loads variables from your .env file
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL, // Your connection string with pooler
+    url: process.env.DATABASE_URL || "https://fclvkmfausfiseoukhax.supabase.co", // Your connection string with pooler
   },
   schema: './prisma/schema.prisma', // Optional, as this is the default
 })
