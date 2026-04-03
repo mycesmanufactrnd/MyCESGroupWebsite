4e4ef958bf9e6a34526034598ff68110714d7006
mycesmanufactrnd <mycesmanufactrnd@gmail.com>
2026-04-03 09:19:48 +0800

    try

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://fclvkmfausfiseoukhax.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjbHZrbWZhdXNmaXNlb3VraGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDM5MjEsImV4cCI6MjA4ODU3OTkyMX0.De--AmkuSsAEAAMoPsFMrnJyeFuUrxHId4BxPiij8JY";
+
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL || SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || SUPABASE_ANON_KEY
 );
